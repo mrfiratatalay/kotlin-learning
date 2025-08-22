faffe172c54c3ab69979bdc027a62fc39cfb26ae
Fırat Atalay <firatatalay.cs@gmail.com>
2025-08-22 20:39:46 +0300

    Start to learn Kotlin

--- 001/app/src/main/java/com/firatatalay/myapplication/MainActivity.kt
+++ 001/app/src/main/java/com/firatatalay/myapplication/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.firatatalay.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,4 +48,20 @@ fun GreetingPreview() {
     MyApplicationTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun PacktPublishing(bookName: String) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(Color.Green),
+        text = "Title of the book is: $bookName")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PacktPublishingPreview() {
+    PacktPublishing("Android Development with Kotlin")
 }
