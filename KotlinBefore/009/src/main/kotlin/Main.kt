38c0c9e6288666ac7a385dda2d53c26384411031
Fırat Atalay <firatatalay.cs@gmail.com>
2025-08-30 00:56:24 +0300

    Practice: Kotlin Fundamentals

--- KotlinBefore/009/src/main/kotlin/Main.kt
+++ KotlinBefore/009/src/main/kotlin/Main.kt
@@ -19,6 +19,58 @@ fun printNotificationSummary(numberOfMessages: Int) {
 */
 
 
+/*
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    if (age < 0 || age > 100) {
+        return -1
+    } else if (age <= 12) {
+        return 15
+    } else if (age in 13..60) {
+        if (isMonday) {
+            return 25
+        }
+        return 30
+    } else {
+        return 20
+    }
+
+}
+*/
+
+
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    return when(age) {
+        in 0..12 -> 15
+        in 13..60 -> if (isMonday) 25 else 30
+        in 61..100 -> 20
+        else -> -1
+    }
+}
+
 
 
 
