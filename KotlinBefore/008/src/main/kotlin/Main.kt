053937c9bf9c2614fc94745a91c439a99ba4c034
Fırat Atalay <firatatalay.cs@gmail.com>
2025-08-30 00:11:21 +0300

    Use function types and lambda expressions in Kotlin

--- KotlinBefore/008/src/main/kotlin/Main.kt
+++ KotlinBefore/008/src/main/kotlin/Main.kt
@@ -164,16 +164,113 @@ val treat = {
 
 
 
-
+/*
 fun main() {
-    val coins: (Int) -> String = { quantity ->
-        "$quantity quarters"
+    val coins: (Int) -> String = {
+        "$it quarters"
     }
 
     val treatFunction = trickOrTreat(false,coins)
     val trickFunction = trickOrTreat(true, null)
     treatFunction()
     trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if(isTrick) {
+        return trick
+    } else {
+        if(extraTreat != null) {
+            println(extraTreat(5))
+        }
+        return treat
+    }
+}
+
+val trick = {
+    println("No treats!")
+}
+
+val treat = {
+    println("Have a treat!")
+}
+
+*/
+
+
+
+
+/*
+fun main() {
+    val treatFunction = trickOrTreat(false, { "$it quarters"})
+    val trickFunction = trickOrTreat(true, null)
+    treatFunction()
+    trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if(isTrick) {
+        return trick
+    } else {
+        if(extraTreat != null) {
+            println(extraTreat(5))
+        }
+        return treat
+    }
+}
+
+val trick = {
+    println("No treats!")
+}
+
+val treat = {
+    println("Have a treat!")
+}
+
+*/
+
+
+
+
+/*
+fun main() {
+    val treatFunction = trickOrTreat(false) { "$it quarters"}
+    val trickFunction = trickOrTreat(true, null)
+    treatFunction()
+    trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if(isTrick) {
+        return trick
+    } else {
+        if(extraTreat != null) {
+            println(extraTreat(5))
+        }
+        return treat
+    }
+}
+
+val trick = {
+    println("No treats!")
+}
+
+val treat = {
+    println("Have a treat!")
+}
+
+*/
+
+
+
+fun main() {
+    val treatFunction = trickOrTreat(false) { "$it quarters"}
+    val trickFunction = trickOrTreat(true, null)
+    repeat(4){
+        treatFunction()
+    }
+    trickFunction()
+
 }
 
 fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
