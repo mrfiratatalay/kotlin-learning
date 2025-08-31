f0d643f61ebca2ceb92518ef7e4c336e0ff4796c
Fırat Atalay <firatatalay.cs@gmail.com>
2025-08-31 05:45:39 +0300

    Create an interactive Dice Roller app

--- KotlinBefore/010/app/src/main/java/com/firatatalay/diceroller/MainActivity.kt
+++ KotlinBefore/010/app/src/main/java/com/firatatalay/diceroller/MainActivity.kt
@@ -9,11 +9,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
